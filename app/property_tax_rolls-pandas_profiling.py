2e3c2bff9580c521a9476c34da91e6afa620d31c
David <daericks@gmail.com>
2019-08-23 12:26:28 -0400

    ugh

--- app/property_tax_rolls-pandas_profiling.py
+++ app/property_tax_rolls-pandas_profiling.py
@@ -4,4 +4,7 @@ import pandas_profiling
 
 df = pd.read_csv('/Users/daviderickson/projects/datasf/data/Assessor_Historical_Secured_Property_Tax_Rolls.csv')
 
-df.profile_report(style={'full_width':True})
+# df.profile_report(style={'full_width':True})
+
+profile = df.profile_report()
+profile.to_file(output_file="pandas_profile-output.html")
